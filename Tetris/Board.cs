6b71e7332e057b89d748efcbf19638cfa4be8196
Bezerra <pabezerra96@gmail.com>
2020-12-30 20:18:46 +0000

    Add Board.cs

---
+++ Tetris/Board.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Tetris
+{
+    public class Board
+    {
+        private int Height;
+        private int Width;
+        private ConsoleColor[,] boardMatrix;
+
+        public Board(int w = 10, int h = 20)
+        {
+            Width = w;
+            Height = h;
+            boardMatrix = new ConsoleColor[Width, Height];
+            for (int x = 0; x < Width; x++)
+                for (int y = 0; y < Height; y++)
+                    boardMatrix[x,y] = ConsoleColor.Black;
+        }
+
+        public bool IsTileFree(int X, int Y)
+        {
+            if (boardMatrix[X, Y] == ConsoleColor.Black)
+                return true;
+            return false;
+        }
+
+        public void DeleteLine(int y)
+        {
+            // move rows down
+            for (; y > 0; y--)
+                for (int x = 0; x < Width; x++)
+                    boardMatrix[x, y] = boardMatrix[x, y-1];
+            
+            // clear top row
+            for (int x = 0; x < Width; x++)
+                boardMatrix[x, 0] = ConsoleColor.Black;
+        }
+
+        public void DeleteCompleteLines()
+        {
+            for (int y = 0; y < Height; y++)
+                for (int x = 0; x < Width; x++)
+                {
+                    // if tile is free, go to next line
+                    if (IsTileFree(x, y))
+                        continue;
+                    // if whole line is occupied, delete this line
+                    DeleteLine(y);
+                }
+        }
+
+    }
+}
