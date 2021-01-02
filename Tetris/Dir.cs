f2a575926f6ed796acf9eedf322db1ace3eb9464
Bezerra <pabezerra96@gmail.com>
2021-01-02 19:38:12 +0000

    Add Doc-comment to Dir

--- Tetris/Dir.cs
+++ Tetris/Dir.cs
@@ -1,13 +1,37 @@
 namespace Tetris
 {
+    /// <summary>
+    /// An enumerator defining possible actions to be performed by the user.
+    /// </summary>
     public enum Dir
     {
+        /// <summary>
+        /// Moves up or rotates 90 degrees clockwise.
+        /// </summary>
         Up,
+        /// <summary>
+        /// Moves down faster.
+        /// </summary>
         Down,
+        /// <summary>
+        /// Moves left.
+        /// </summary>
         Left,
+        /// <summary>
+        /// Moves right.
+        /// </summary>
         Right,
+        /// <summary>
+        /// Confirm selection.
+        /// </summary>
         Enter,
+        /// <summary>
+        /// Rotates 90 degrees clockwise.
+        /// </summary>
         Rot,
+        /// <summary>
+        /// Does nothing.
+        /// </summary>
         None
     };
 }
